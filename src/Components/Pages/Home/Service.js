dfe6669d81a277ba6db74fa21aaaaf5da39e260a
AmanatUMahmud <amanatmahmud@gmail.com>
2022-11-08 22:44:14 +0600

    SERVICE image with photo-view

--- src/Components/Pages/Home/Service.js
+++ src/Components/Pages/Home/Service.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import { FaStar,FaUserAlt } from "react-icons/fa";
+import { FaStar, FaUserAlt } from "react-icons/fa";
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 const Service = ({ service }) => {
-    const {title,description,img,rating,customers,price} = service
+    const { title, description, img, rating, customers, price } = service
     // console.log(typeof(img));
     return (
         <div className='col-span-1 mx-auto'>
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src={img} alt="" className='w-full h-full'/></figure>
+                <figure><PhotoProvider>
+                    {/* THE IMAGe which will be shown when clicked*/}
+                    <PhotoView src={img}>
+                        {/* normal image in ui */}
+                        <img src={img} alt="" />
+                    </PhotoView>
+                </PhotoProvider></figure>
                 <div className="card-body">
+                    {/* title */}
                     <h2 className="card-title">{title} Photography</h2>
-                    <p>{description.slice(0,100)+'...'}</p>
+                    {/* description */}
+                    <p>{description.slice(0, 100) + '...'}</p>
+                    {/* Customers, rating */}
                     <div className='flex justify-between items-center text-lg'>
                         <div>
-                        <p className='flex justify-items-center items-center'><FaStar className='mr-2'style={{color:'yellow'}}></FaStar>{rating}</p>
+                            <p className='flex justify-items-center items-center'><FaStar className='mr-2' style={{ color: 'yellow' }}></FaStar>{rating}</p>
                         </div>
                         <div>
-                        <p className='flex justify-items-center items-center'><FaUserAlt className='mr-2'></FaUserAlt>{customers}k</p>
+                            <p className='flex justify-items-center items-center'><FaUserAlt className='mr-2'></FaUserAlt>{customers}k</p>
                         </div>
                     </div>
                     <div>
+                        {/* Price */}
                         <p className='text-2xl font-bold'>Price: ${price}</p>
                     </div>
+                    {/* view details buttonbutton */}
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">View Details</button>
                     </div>
