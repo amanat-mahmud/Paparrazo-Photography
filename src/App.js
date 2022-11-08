34637fb88682598958dac4b7133c030051e87b03
AmanatUMahmud <amanatmahmud@gmail.com>
2022-11-08 17:43:33 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
-
+import router from './Context/Router/Routes';
 function App() {
   return (
-    <div >
-      
+    <div className='bg-black w-full h-full text-white'>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </div>
   );
 }
