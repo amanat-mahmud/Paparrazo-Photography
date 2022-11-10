c1f87a1064882787ab8706c79e79334da4ba2d49
AmanatUMahmud <amanatmahmud@gmail.com>
2022-11-10 16:24:51 +0600

    Routes.js 23 line fixed

--- src/Routes/Router/Routes.js
+++ src/Routes/Router/Routes.js
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
-                loader: () => fetch('http://localhost:5000'),
+                loader: () => fetch('https://paparazzo-photography-server.vercel.app'),
             },
             {
                 path: '/services',
